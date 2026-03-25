f41a043ed18b83b43be30dff7eaffecef547c66d
Creatorpoint <pk4030794@gmail.com>
2026-03-25 15:00:04 +0530

    Update web.py

--- web.py
+++ web.py
@@ -7,9 +7,8 @@ PORT = int(os.environ.get("PORT", 10000))
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Prime Analytics Bot Running ✅</h1>")
+        self.wfile.write(b"Bot Running ✅")
 
 def run():
     HTTPServer(("0.0.0.0", PORT), Handler).serve_forever()
