0427ec811e789bd6a7d53734800ffe2dc6116fa7
Creatorpoint <pk4030794@gmail.com>
2026-03-25 15:01:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 
 OWNER_ID = int(os.getenv("OWNER_ID"))
+
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
