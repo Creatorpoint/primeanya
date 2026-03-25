605dfb9a6f520ccbdf7d7dc78a7a6cf040f852a7
Creatorpoint <pk4030794@gmail.com>
2026-03-25 15:00:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import asyncio
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-from handlers import start, admin, stats, group_events
+from handlers import register_all_handlers
 import web
+from web_panel.app import start_web
 
 app = Client(
-    "PrimeAnalyticsBot",
+    "PrimeBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
+register_all_handlers(app)
+
 async def main():
     await app.start()
     print("✅ Bot Started")
+
+    start_web()  # web panel start
 
     await asyncio.Event().wait()
 
